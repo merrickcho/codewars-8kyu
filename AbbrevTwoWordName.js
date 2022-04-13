c015b325db666635c5e0a88b4a35d3069afa5f70
Merrick Cho <merrick.cho@gmail.com>
2022-04-13 00:34:02 -0700

    Solution for Abbreviate two word name

---
+++ AbbrevTwoWordName.js
@@ -0,0 +1,6 @@
+// Abbreviate a Two Word Name
+// https://www.codewars.com/kata/57eadb7ecd143f4c9c0000a3
+
+function abbrevName(name){
+    return (name.split(' ')[0][0]+"."+name.split(' ')[1][0]).toUpperCase()
+  }
